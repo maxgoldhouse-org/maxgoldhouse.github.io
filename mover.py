0f9ee5321a09c68a5350e915d5c4254e69230b26
maxgoldhouse <dklongley@maxgoldhouse.com>
2015-07-07 23:43:14 +0100

    Update mover.py

--- mover.py
+++ mover.py
@@ -14,18 +14,3 @@ for src_dir, dirs, files in os.walk(root_src_dir):
         if os.path.exists(dst_file):
             os.remove(dst_file)
         shutil.move(src_file, dst_dir)
-
-root_src_dir = './uploads/versions'
-root_dst_dir = './static/uploads/versions'
-
-for src_dir, dirs, files in os.walk(root_src_dir):
-    dst_dir = src_dir.replace(root_src_dir, root_dst_dir)
-    if not os.path.exists(dst_dir):
-        os.mkdir(dst_dir)
-    for file_ in files:
-        src_file = os.path.join(src_dir, file_)
-        dst_file = os.path.join(dst_dir, file_)
-        if os.path.exists(dst_file):
-            os.remove(dst_file)
-        shutil.move(src_file, dst_dir)
-        
